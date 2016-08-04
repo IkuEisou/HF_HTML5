16a4c57470b4f770e8fc223f30346486d846668b
IkuEisou <yuyc@cn.fujitsu.com>
2016-08-04 23:59:37 +0800

    完成用户添加歌曲功能

--- JavaScript/storelist.js
+++ JavaScript/storelist.js
@@ -28,8 +28,32 @@ function loadPlaylist( ) {
 	if (playlistArray != null) {
 		for (var i = playlistArray.length - 1; i >= 0; i--) {
 			var li = document.createElement("li");
-			li.innerHTML = playlistArray[i];
+			var rmbtn = document.createElement("button");
+			var rmtext = document.createTextNode("x");
+			var song = document.createTextNode(playlistArray[i]+"  ");
+			rmbtn.setAttribute("class", "remove");
+			rmbtn.setAttribute("id", i);
+			rmbtn.appendChild(rmtext);
+			li.appendChild(song);
+			li.appendChild(rmbtn);
+			// li.innerHTML = playlistArray[i] ;
 			ul.appendChild(li);
+			console.log(li.outerHTML);
 		}
+
+	    // var buttons = document.getElementsByClassName('remove');
+     //    for (var i=0; i < buttons.length; i++) {
+     //      buttons[i].addEventListener('click', remove(i, "playlist"));
+     //    }
 	}
+}
+
+function remove(index, list) {
+    var songlist = getStoreArray();
+    songlist.splice(index, 1);
+    localStorage.setItem(list, JSON.stringify(songlist));
+
+    // loadPlaylist();
+
+    return false;
 }
